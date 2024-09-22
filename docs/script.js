d389c2b0d599af575d138694a557984c9fddd82e
Prakit <saratgogoi78@gmail.com>
2024-09-22 16:40:02 +0530

    Create script.js

---
+++ docs/script.js
@@ -0,0 +1,12 @@
+// Get the button elements
+const getStartedButton = document.getElementById('get-started');
+const getExploitsButton = document.getElementById('get-exploits');
+
+// Add event listeners to the buttons
+getStartedButton.addEventListener('click', () => {
+    window.location.href = 'https://prakit1234.github.io/UTPALX/';
+});
+
+getExploitsButton.addEventListener('click', () => {
+    window.location.href = 'https://prakit1234.github.io/UTPALX/docs2/';
+});
